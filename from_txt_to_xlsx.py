ca56e3724e95faee7992c19dcaa416ba7ce6f597
Krzysztof <35111684+kjurkiew@users.noreply.github.com>
2023-09-11 22:40:55 +0200

    Update from_txt_to_xlsx.py
    Type Hints: Added type hints for function parameters and return values to enhance readability and IDE support.
    
    Naming: Renamed functions and variables for clarity and adherence to Python naming conventions.
    
    Constants: Introduced NUM_COLUMNS as a constant for better maintainability.
    
    os.path.join: Improved file path handling with os.path.join for cross-platform compatibility.
    
    Comments: Added docstrings to functions to explain their purpose and parameters.
    
    File Handling: Checked for file extensions when listing files and ensured .xlsx for output files.
    
    PEP 8: Enhanced code formatting and alignment with PEP 8 guidelines.

--- from_txt_to_xlsx.py
+++ from_txt_to_xlsx.py
@@ -1,55 +1,100 @@
 import re
 import os
 import xlsxwriter
+from typing import List
+
+# Constants
+NUM_COLUMNS = 8
 
 
-def taking_cities():
-    # Finding all files in directory, and taking their names 
-   cities = [filename for filename in os.listdir('nowe_miasta/')]
-    return cities
+def get_city_files(directory: str) -> List[str]:
+    """Get a list of city files in the specified directory.
+
+    Args:
+        directory (str): The directory to search for city files.
+
+    Returns:
+        List[str]: A list of city file names.
+    """
+    return [filename for filename in os.listdir(directory) if filename.endswith(".txt")]
 
 
-def opening_file(file_name):
-    # Opening and reading file
-    with open('nowe_miasta/' + file_name, 'r') as file:
-        city_data = file.read()
-    return city_data
+def read_city_data(file_path: str) -> str:
+    """Read and return the content of a city file.
+
+    Args:
+        file_path (str): The path to the city file.
+
+    Returns:
+        str: The content of the city file as a string.
+    """
+    with open(file_path, "r") as file:
+        return file.read()
 
 
-def find_values(city_data):
-    # Values are separated by space, using regex to find all values
-    values = re.findall(r'-*\w\S*', city_data)
-    return [float(value) for value in values]
+def extract_values(city_data: str) -> List[float]:
+    """Extract numerical values from city data.
+
+    Args:
+        city_data (str): The city data as a string.
+
+    Returns:
+        List[float]: A list of extracted numerical values.
+    """
+    return [float(value) for value in re.findall(r"-*\w\S*", city_data)]
 
 
-def creating_lines(values):
-    # Creating table which has 8 item tables. Taking first 8 items from 46 items in values
-    new_values = [['Godz.', 'Mies.', 'Dzien', 'Godz. UTC', 'Temp. term. such.', 'Wilg. wzgl.', 'Zaw. wilg.', 'Wiatr']]
-    j = 0
-    k = 1
-    for i in values:
-        if j == 0:
-            i = float(i)
-            new_values.append([i])
-            j = j + 1
-        elif j < 8:
-            i = float(i)
-            new_values[k].append(i)
-            j = j + 1
-        elif j == 45:
-            j = 0
-            k = k + 1
+def creating_lines(values: List[float]) -> List[List[float]]:
+    """Create a table from numerical values.
+
+    Args:
+        values (List[float]): A list of numerical values.
+
+    Returns:
+        List[List[float]]: A list of lists representing the table.
+    """
+    table = [
+        [
+            "Godz.",
+            "Mies.",
+            "Dzien",
+            "Godz. UTC",
+            "Temp. term. such.",
+            "Wilg. wzgl.",
+            "Zaw. wilg.",
+            "Wiatr",
+        ]
+    ]
+    column_index = 0
+    row_index = 1
+
+    for value in values:
+        if column_index == 0:
+            value = float(value)
+            table.append([value])
+            column_index += 1
+        elif column_index < 8:
+            value = float(value)
+            table[row_index].append(value)
+            column_index += 1
+        elif column_index == 45:
+            column_index = 0
+            row_index += 1
         else:
-            j = j + 1
-    return new_values
+            column_index += 1
+
+    return table
 
 
-def saving_to_new_file(data, file_name):
-    # Changing filename txt into xlsx
-    file_name = file_name[:-3] + 'xlsx'
+def save_to_excel(data: List[List[float]], output_filename: str) -> None:
+    """Save data to an Excel file.
 
-    # Create a workbook and add a worksheet
-    workbook = xlsxwriter.Workbook('excele/' + file_name)
+    Args:
+        data (List[List[float]]): The data to be saved as a table.
+        output_filename (str): The name of the output Excel file.
+    """
+    output_filename = output_filename[:-4] + ".xlsx"  # Ensure it's an xlsx file
+    workbook = xlsxwriter.Workbook(os.path.join("excele", output_filename))
     worksheet = workbook.add_worksheet()
 
     # Start from the first cell. Rows and columns are zero indexed
@@ -66,23 +111,23 @@ def saving_to_new_file(data, file_name):
         worksheet.write(row, col + 5, item_6)
         worksheet.write(row, col + 6, item_7)
         worksheet.write(row, col + 7, item_8)
-        worksheet.write(row, col + 8, '=$B$1 - E' + str(row+1))
+        worksheet.write(row, col + 8, "=$B$1 - E" + str(row + 1))
         row += 1
 
     # Writing basic - the same for all worksheets
-    worksheet.write(0, 0, 'Temp. wew')
+    worksheet.write(0, 0, "Temp. wew")
     worksheet.write(0, 1, 18)
-    worksheet.write(3, 8, 'Stopniogodz.')
+    worksheet.write(3, 8, "Stopniogodz.")
 
     workbook.close()
 
 
-if __name__ == '__main__':
-    cities = taking_cities()
+if __name__ == "__main__":
+    city_directory = "nowe_miasta/"
+    cities = get_city_files(city_directory)
 
-    for i in cities:
-        file_name = i
-        city_data = opening_file(file_name)
-        values = find_values(city_data)
+    for city_file in cities:
+        city_data = read_city_data(os.path.join(city_directory, city_file))
+        values = extract_values(city_data)
         data = creating_lines(values)
-        saving_to_new_file(data, file_name)
+        save_to_excel(data, city_file)
